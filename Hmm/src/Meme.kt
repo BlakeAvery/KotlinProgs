e8159f82b764f1978f81f8cce4c0bbb59a4a69b2
Jared Simmons <simmons.jared76@gmail.com>
2018-02-13 22:48:31 -0500

    Added try-catch block to Meme class

--- Hmm/src/Meme.kt
+++ Hmm/src/Meme.kt
@@ -1,3 +1,6 @@
+import java.lang.System.exit
+//import kotlin.system.exitProcess i dunno, this worked that one time without having to import anything???
+
 class Meme constructor(manko: Int) {
     private var bussy: Int
     init{
@@ -9,13 +12,19 @@ class Meme constructor(manko: Int) {
     override fun toString(): String {
         return bussy.toString() + " is a number!"
     }
-    fun Mather(op: String, numb: Int): Int {
-        when(op) {
-            "+" -> return bussy + numb
-            "-" -> return bussy - numb
-            "*" -> return bussy * numb
-            "/" -> return bussy / numb
-            else -> throw IllegalArgumentException() //Try-catch block may work here, but I'm legit too lazy right now to implement.
+    fun Mather(op: String, numb: Int): String {
+        try {
+            when(op) {
+                "+" -> return (bussy + numb).toString()
+                "-" -> return (bussy - numb).toString()
+                "*" -> return (bussy * numb).toString()
+                "/" -> return (bussy / numb).toString()
+                else -> throw IllegalArgumentException() //Try-catch block may work here, but I'm legit too lazy right now to implement.
+            }
+        } catch(e: IllegalArgumentException) {
+            return "Fun fact, I'm too lazy to implement checking for proper input in main. Oh well."
+            exit(19)
         }
+
     }
 }
