f1f3a66081286ce324b5984131d3bc2feca70ebd
Jared Simmons <simmons.jared76@gmail.com>
2018-02-14 22:33:08 -0500

    Fixed bug in Coffee.kt where price setter wouldn't actually set price

--- Coffee/src/Coffee.kt
+++ Coffee/src/Coffee.kt
@@ -2,8 +2,8 @@ import java.lang.System.exit //TODO: Figure out kotlin exit function and how it 
 
 fun main(args: Array<String>) {
     var DrinkList = ArrayList<Drink>()
-    var x = 0
-    var input = 0
+    var x: Int
+    var input: Int
     while(true) {
         print("Enter drink name (or break to exit): ")
         var nom = readLine() ?: ""
@@ -23,15 +23,16 @@ fun main(args: Array<String>) {
                 input = readLine()?.toInt() ?: 0
                 var drink = DrinkList.get(input)
                 println("Enter new price (current drink is $drink)")
-                var newPrice = readLine()?.toFloat() ?: 0.0
-                DrinkList.set(input, drink)
+                var newPrice: Float = readLine()?.toFloat() ?: 0.0F
+                drink.price = newPrice
+                DrinkList[input] = drink
                 println("New price $newPrice set")
             }
             2 -> {
                 println("Get info of which drink?")
                 input = readLine()?.toInt() ?: 0
                 try {
-                    val infoGetter = DrinkList.get(input)
+                    val infoGetter = DrinkList[input]
                     println(infoGetter)
                 } catch(e: IndexOutOfBoundsException) {
                     println("Error: invalid input")
